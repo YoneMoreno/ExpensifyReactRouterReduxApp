e076b33992a57031a620ef2b6b2c1e8532e86a1c
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-04 16:28:52 +0100

    We create our first route!

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const ExpensesDashboardPage = () => (
+    <div>
+        This is our new dashboard component ;=)!
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <Route path='/' component={ExpensesDashboardPage}/>
+    </BrowserRouter>
+);
+
 
 ReactDOM.render(
-    <p>This is my boilerplate</p>,
+    routes,
     document.getElementById('app'));
