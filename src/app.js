6bd25f39e031e518c70869c9dded98dc86e0a218
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-04 17:06:52 +0100

    We have 4 Link to go via client rendering to each page

--- src/app.js
+++ src/app.js
@@ -34,6 +34,10 @@ const NotFoundPage = () => (
 const Header = () => (
     <header>
         <h1>Expensify</h1>
+        <Link to='/'>Index </Link>
+        <Link to='/create'>Create Expense </Link>
+        <Link to='/edit'>Edit Expense </Link>
+        <Link to='/help'>Get Help </Link>
     </header>
 );
 
